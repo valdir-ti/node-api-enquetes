5c0347947aabd606e5d41c644730a58dfb676a95
Valdir Silva <valdir.ti@gmail.com>
2022-11-17 08:12:31 -0300

    test: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
+import { httpRequest, httpResponse } from "../protocols/http"
 export class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: httpRequest): httpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
@@ -11,6 +12,10 @@ export class SignUpController {
         statusCode: 400,
         body: new Error("Missing Param: email"),
       }
+    }
+    return {
+      statusCode: 200,
+      body: {},
     }
   }
 }
