b2876b0cd27b04a05ebc61738eb4d94cda91ffb6
Valdir Silva <valdir.ti@gmail.com>
2022-11-19 16:00:49 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -88,4 +88,17 @@ describe("DbAddAccount Usecase", () => {
       password: "hashed_password",
     })
   })
+  test("Should throws if AddAccountRepopsitory throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest
+      .spyOn(addAccountRepositoryStub, "add")
+      .mockReturnValueOnce(new Promise((_, reject) => reject(new Error())))
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
