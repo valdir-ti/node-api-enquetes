a388d5d6339c4e35a48ff638f2d2e7cef71b5433
Valdir Silva <valdir.ti@gmail.com>
2022-11-18 22:40:10 -0300

    refactor: transform handle to async

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -17,7 +17,7 @@ export class SignUpController implements Controller {
     this.addAccount = addAccount
   }
 
-  handle(httpRequest: httpRequest): httpResponse {
+  async handle(httpRequest: httpRequest): Promise<httpResponse> {
     try {
       const requiredFields = [
         "email",
@@ -43,7 +43,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError("email"))
       }
 
-      const account = this.addAccount.add({
+      const account = await this.addAccount.add({
         name,
         email,
         password,
