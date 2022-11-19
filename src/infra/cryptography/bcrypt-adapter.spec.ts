adb09c9a039636f2cf21dcbbc07ef013843a318b
Valdir Silva <valdir.ti@gmail.com>
2022-11-19 20:55:33 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -2,12 +2,25 @@ import bcrypt from "bcrypt"
 
 import { BcryptAdapter } from "./bcrypt-apadpter"
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise<string>(resolve => resolve("hash"))
+  },
+}))
+
 describe("Bcrypt Adapter", () => {
   test("Should call bcrypt with correct values", async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, "hash")
-    sut.encrypt("any_value")
+    await sut.encrypt("any_value")
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt)
+  })
+
+  test("Should return a hash on success", async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt("any_value")
+    expect(hash).toBe("hash")
   })
 })
