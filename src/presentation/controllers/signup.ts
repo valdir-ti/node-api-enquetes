ef663e0f04d2f1501e365440f5c28799de151b44
Valdir Silva <valdir.ti@gmail.com>
2022-11-18 21:33:03 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,13 @@
-import { httpRequest, httpResponse } from "../protocols/http"
-import { MissingParamError } from "../errors/missing-param-error"
-import { InvalidParamError } from "../errors/invalid-param-error"
-import { badRequest } from "../helpers/http-helper"
 import { Controller } from "../protocols/controller"
+import { httpRequest, httpResponse } from "../protocols/http"
 import { EmailValidator } from "../protocols/email-validator"
+
+import { badRequest } from "../helpers/http-helper"
+
+import { ServerError } from "../errors/server-error"
+import { InvalidParamError } from "../errors/invalid-param-error"
+import { MissingParamError } from "../errors/missing-param-error"
+
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
 
@@ -12,23 +16,35 @@ export class SignUpController implements Controller {
   }
 
   handle(httpRequest: httpRequest): httpResponse {
-    const requiredFields = ["email", "name", "password", "passwordConfirmation"]
+    try {
+      const requiredFields = [
+        "email",
+        "name",
+        "password",
+        "passwordConfirmation",
+      ]
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
 
-    if (!isValid) {
-      return badRequest(new InvalidParamError("email"))
-    }
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"))
+      }
 
-    return {
-      statusCode: 200,
-      body: {},
+      return {
+        statusCode: 200,
+        body: {},
+      }
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      }
     }
   }
 }
